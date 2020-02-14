3eae5a66cfac4674f0f501223c543c17c61c571b
niel905g <niel905g@edu.easj.dk>
2020-02-14 11:11:18 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+    <img src="{{imageUrl}}" />
+    <div>
+    <button
+    class="btn btn-primary btn-lg"
+    [class.disabled]="!isValid">Submit</button>
+    </div>
+    <p></p>
+   <div>
+   <button type="button" class="btn btn-danger" (click)="onClickMe($event)">Danger</button>
+</div>
+    `
 })
 export class AppComponent {
-  title = 'bindings';
+  isValid = false;
+  title = 'Demo of Angular 2 bindings';
+  imageUrl = "https://www.w3schools.com/html/pic_mountain.jpg";
+  onClickMe($event) {
+    console.log("Watch out",$event)
+  }
 }
